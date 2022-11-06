341d065772dcd0fc157f9749a90beaeba3e5079c
gosueep <eugin03@gmail.com>
2022-11-06 00:06:19 -0400

    testget

--- server/main.py
+++ server/main.py
@@ -8,8 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    return "Hello {} NEW!".format(name)
 
+@app.route("/testGet")
+def test_get():
+    return "ANOTHER GET"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
