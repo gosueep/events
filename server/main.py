77da0ce21e2f0db8e0c59833e2a202e2117321cd
gosueep <eugin03@gmail.com>
2022-11-06 01:52:26 -0400

    bruh

--- server/main.py
+++ server/main.py
@@ -1,14 +1,48 @@
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+import pg8000
 import os
-
 from flask import Flask
 
 app = Flask(__name__)
+conn = Connector()
+
+def getconn() -> pg8000.connections.Connection:
+    conn: pg8000.connections.Connection = conn.connect(
+        "hacknc22:us-central1:events-data",
+        "pg8000",
+        user="postgres",
+        password="MZ{s.CxTb|JjvLBy",
+        db="events-data"
+    )
+    return conn
+
+pool = sqlalchemy.create_engine(
+    "pg8000+pg8000://",
+    creator=getconn,
+)
+
+# insert statement
+insert_stmt = sqlalchemy.text(
+    "INSERT INTO my_table (id, title) VALUES (:id, :title)",
+)
+
+with pool.connect() as db_conn:
+    # insert into database
+    # db_conn.execute(insert_stmt, id="book1", title="Book One")
+
+    # query database
+    result = db_conn.execute("SELECT * from events").fetchall()
+
+    # Do something with the results
+    for row in result:
+        print(row)
 
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {} NEW!".format(name)
+    return "Hello {}".format(result)
 
 @app.route("/testGet")
 def test_get():
