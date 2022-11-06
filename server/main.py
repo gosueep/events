9a6690cc703dc4a294b777a04e39cbe61f2c446d
gosueep <eugin03@gmail.com>
2022-11-06 08:16:53 -0500

    change 35

--- server/main.py
+++ server/main.py
@@ -32,7 +32,7 @@ def update_location():
     d = request.json 
     with pool.connect() as db_conn:
         # insert into database
-        db_conn.execute("INSERT INTO location (id, lat, long, pos) VALUES (:id, lat, long, point(:long, :lat))", id=d['device_id'], lat=d['lat'], long=d['long'], long=d['long'], lat=d['lat'])
+        # db_conn.execute("INSERT INTO location (id, lat, long, pos) VALUES (:id, lat, long, point(:long, :lat))", id=d['device_id'], lat=d['lat'], long=d['long'], long=d['long'], lat=d['lat'])
 
 app.post("/rsvp")
 def rsvp():
